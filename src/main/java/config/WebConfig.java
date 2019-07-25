64a1d3ff317e87cddcd8dd90c025e5a50e66b8ca
Demid5 <demkozlov@yandex.ru>
2019-07-25 11:54:39 +0700

    fdsl

--- src/main/java/config/WebConfig.java
+++ src/main/java/config/WebConfig.java
@@ -3,23 +3,14 @@ package config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("controller")
-public class WebConfig implements WebMvcConfigurer {
-
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/res/**").addResourceLocations("/res/");
-    }
+public class WebConfig {
 
     @Bean
     ViewResolver viewResolver() {
@@ -28,5 +19,4 @@ public class WebConfig implements WebMvcConfigurer {
         viewResolver.setPrefix("/WEB-INF/pages/");
         return viewResolver;
     }
-
 }
