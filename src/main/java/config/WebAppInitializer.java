b9faa1cbe533f0c41cc3d6e4baca1f173db0f9eb
Demid5 <demkozlov@yandex.ru>
2019-08-05 19:36:32 +0700

    fsdf

--- src/main/java/config/WebAppInitializer.java
+++ src/main/java/config/WebAppInitializer.java
@@ -5,24 +5,18 @@ import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatche
 
 import javax.servlet.Filter;
 
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
-public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
-    @Override
-    protected Class<?>[] getRootConfigClasses() {
-        return new Class[0];
-    }
+public class WebAppInitializer extends SpringBootServletInitializer {
 
     @Override
-    protected Class<?>[] getServletConfigClasses() {
-        return new Class[]{WebConfig.class};
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(WebConfig.class);
     }
 
-    @Override
-    protected String[] getServletMappings() {
-        return new String[]{"/"};
-    }
 
-   protected Filter[] getServletFilters() {
+    protected Filter[] getServletFilters() {
         CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
         characterEncodingFilter.setEncoding("UTF-8");
         characterEncodingFilter.setForceEncoding(true);
